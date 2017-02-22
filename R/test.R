89f13bc57a2a7ed1446ab02317dfdeb637233375
Yeuk Yu Lee <yeukyul@andrew.cmu.edu>
2017-02-22 10:50:06 -0500

    updated gg_resX

--- R/test.R
+++
@@ -1,8 +0,0 @@
-
-# sample lm for testing lindia 
-library(MASS)
-data(Cars93)
-cars_lm <- lm(Rev.per.mile ~ Passengers + Length + RPM, data = Cars93)
-cars_lm_2 <- lm(Rev.per.mile ~ Passengers + Length + RPM + Origin, data = Cars93)
-cars_lm_3 <- lm(Rev.per.mile ~ Passengers + Length + RPM + Manufacturer + Weight * Length, data = Cars93)
-cars_lm_4 <- lm(Rev.per.mile ~ Passengers + Length + RPM + Manufacturer + Type + Make * Origin , data = Cars93)
