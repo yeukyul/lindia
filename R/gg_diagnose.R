0c6d157ea2afaa6dba074f465cdaa3c556b532d7
Yeuk Yu Lee <yeukyul@andrew.cmu.edu>
2017-04-20 22:42:44 -0400

    updated name of plot.all param

--- R/gg_diagnose.R
+++ R/gg_diagnose.R
@@ -21,7 +21,7 @@
 #' cars_lm <- lm(Price ~ Passengers + Length + RPM + Origin, data = Cars93)
 #' gg_diagnose(cars_lm)
 #' # customize which diagnostic plot is included
-#' plots <- gg_diagnose(cars_lm, plotAll = FALSE)
+#' plots <- gg_diagnose(cars_lm, plot.all = FALSE)
 #' names(plots)     # get name of the plots
 #' exclude_plots <- plots[-c(1, 3) ]    #exclude certain diagnostics plots
 #' include_plots <- plots[c(1, 3)]      # include certain diagnostics plots
--- R/gg_resX.R
+++ R/gg_resX.R
@@ -18,7 +18,7 @@
 #' cars_lm <- lm(Price ~ Passengers + Length + RPM + Origin, data = Cars93)
 #' gg_resX(cars_lm)
 #' # customize which diagnostic plot is included by have gg_resX to return a list of plots
-#' plots <- gg_resX(cars_lm, plotAll = FALSE)
+#' plots <- gg_resX(cars_lm, plot.all = FALSE)
 #' names(plots)     # get name of the plots
 #' exclude_plots <- plots[-1 ]    #exclude certain residual plots
 #' include_plots <- plots[1]      # include certain residual plots
