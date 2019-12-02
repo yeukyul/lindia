3ecfb33b31c083de0e55a187feacc5d0de7fc2ac
Aymeric Stamm <aymeric.stamm@math.cnrs.fr>
2019-12-02 22:56:35 +0100

    Add a normal independent sample to compare to in scale-location plot.

--- R/gg_scalelocation.R
+++ R/gg_scalelocation.R
@@ -19,14 +19,27 @@ gg_scalelocation <- function(fitted.lm, method = 'loess', scale.factor = 1, se =
    handle_exception(fitted.lm, "gg_scalelocation")
 
    #obtain stardardized residual and fitted values from fitted.lm
-   fitted_values = fitted(fitted.lm)
-   std_res = rstandard(fitted.lm)
+   fitted_values <- fitted(fitted.lm)
+   std_res <- rstandard(fitted.lm)
+   n <- length(std_res)
+   std_res_th <- rnorm(n)
 
-   df = data.frame(std_res, fitted_values)
-   names(df) = c("sqrt(std_res)", "fitted_values")
-   return (ggplot(data = df, aes(x = fitted_values, y = sqrt(abs(std_res)))) +
-              geom_point(size = scale.factor) +
-              geom_smooth(method = method, se = se, size = scale.factor, color = "indianred3") +
-              ggtitle("Scale-Location Plot") +
-              labs(x="Fitted Values", y = "Sqrt(Standardized Residuals)"))
+   df <- data.frame(
+      sqrt_abs_std_res = sqrt(abs(c(std_res, std_res_th))),
+      fitted_values = rep(fitted_values, times = 2),
+      type = rep(c("Observed Distribution", "Theoretical Distribution"), each = n)
+   )
+
+   ggplot(data = df, aes(x = fitted_values, y = sqrt_abs_std_res, col = type)) +
+      geom_point(size = scale.factor) +
+      geom_smooth(method = method, se = se, size = scale.factor, color = "indianred3") +
+      labs(
+         title = "Scale-Location Plot",
+         x = "Fitted Values",
+         y = "Sqrt(Standardized Residuals)"
+      ) +
+      facet_grid(cols = vars(type)) +
+      theme_bw() +
+      theme(legend.position = "none") +
+      scale_color_viridis_d(option = "cividis")
 }
