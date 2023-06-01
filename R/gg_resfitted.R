b3cffce661f91d913251d3b6255273efb6ff0573
Tony Y <94228508+tonyy0001@users.noreply.github.com>
2023-06-01 12:35:52 +0800

    Update gg_resfitted.R

--- R/gg_resfitted.R
+++ R/gg_resfitted.R
@@ -2,6 +2,10 @@
 #' Generate residual plot of residuals against fitted value
 #'
 #' @param fitted.lm a fitted linear model (i.e. lm, glm) that contains fitted regression
+#' @param method smoothing method of fitted line on scale-location plot.
+#'          eg. "lm", "glm", "gam", "loess", "rlm". See \url{http://docs.ggplot2.org/current/geom_smooth.html}
+#'          for more details.
+#' @param se logical; determines whether se belt should be plotted on plot
 #' @param scale.factor numeric; scales the point size and linewidth to allow customized viewing. Defaults to 1.
 #' @return A ggplot object
 #' @examples library(MASS)
@@ -11,7 +15,7 @@
 #'
 #' @export
 #'
-gg_resfitted <- function(fitted.lm, scale.factor = 1) {
+gg_resfitted <- function(fitted.lm, method = 'loess', scale.factor = 1, se = FALSE) {
 
    handle_exception(fitted.lm, "gg_resfitted")
 
@@ -29,6 +33,7 @@ gg_resfitted <- function(fitted.lm, scale.factor = 1) {
    return (ggplot(data = df, aes(y = residuals, x = fitted_values)) +
               geom_point(size = scale.factor) +
               geom_hline(yintercept = 0, linetype = "dashed", color = "indianred3", size = scale.factor)  +
+              geom_smooth(method = method, se = se, size = scale.factor, color = "indianred3") +
               labs(y = "Residuals", x = "Fitted Values") +
               ylim(-(limit + margin), limit + margin) +
               ggtitle("Residual vs. Fitted Value"))
